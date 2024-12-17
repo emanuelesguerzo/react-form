df9a818fd9079fcb7ac4d2a0c3f2a17046df5de5
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2024-12-17 18:04:01 +0100

    feat: added form to create and delete posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import { useState } from "react";
 
-
 function App() {
   const [posts, setPosts] = useState([]);
-  const [title, setTitle] = useState("")
+  const [newTitle, setNewTitle] = useState("")
+
+  const handleNewPostSubmit = (event) => {
+    event.preventDefault();
+
+    const newArray = [...posts, newTitle];
+    setPosts(newArray);
+    setNewTitle("");
+  }
+
+  const removePost = (postToRemove) => {
+    const newArray = posts.filter(curPost => curPost !== postToRemove);
+    setPosts(newArray);
+  }
 
   return (
     <>
@@ -12,16 +24,31 @@ function App() {
       </header>
 
       <main>
-        <form action="" className="container row">
+        <form action="" className="container row" onSubmit={handleNewPostSubmit}>
           <label htmlFor="PostName">Titolo del Post</label>
           <input
             type="text"
+            placeholder="Scrivi il titolo del tuo Post!"
             id="PostName"
+            value={newTitle}
+            onChange={(event) => setNewTitle(event.target.value)}
           />
           <button type="submit">Crea Post</button>
         </form>
+
+        <ul>
+          {posts.map((curPost, index) => (
+            <li key={index}>
+              <h2>{curPost}</h2>
+              <button
+                onClick={() => {removePost(curPost)}}
+              >
+                <i className="fa-solid fa-trash"></i>
+              </button>
+            </li>
+          ))}
+        </ul>
       </main>
-      
     </>
   )
 }
